d0114b145a479a0e4c01dbf10df67da984ca9a61
sasuke37 <loser6337@gmail.com>
2014-02-04 23:09:01 +0100

    retification de bug

---
+++ backup/connect.php
@@ -0,0 +1,7 @@
+<?php 
+// defini les constante de connection a la base
+define('USER', "root");
+define('PASSWD', "");
+define('SERVER', "localhost");
+define('BASE', "projetplanning")
+ ?>
--- libs/connect.php
+++ libs/connect.php
@@ -1,11 +1,11 @@
 <?php
-	require_once('../includes/config.php')
+	require_once('./includes/config.php');
 
 	function connectBD(){
-		$dns="mysql:dbname=".BASE.";host= ".SERVER;
+		$dns="mysql:dbname=".BASE."host=".SERVER;
 
 		try{
-			$con = new PDO ($dns, USER, PASSWD);
+			$con = new PDO($dns, USER, PASSWD);
 		}
 		catch(PDOExceptin $e) {
 			$erreur = "Erreur de connexion base de données";
